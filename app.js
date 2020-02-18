e99e6e1b1c08a08547029ec0c876237d3d91ad51
권태일 <xodlf0415@jmsight.com>
2020-02-18 13:20:49 +0900

    기본 선 색을 검정색으로 두고 canvas의 context 함수들을 이용해서 선을 그리는 기능

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById('jsCanvas')
 const ctx = canvas.getContext("2d")
 
+canvas.width = 700; // context함수를 이용하기 위해서는 css가 아닌 element에 사이즈를 줘야한다.
+canvas.height = 700;
+
 ctx.strokeStle="#2c2c2c"
 ctx.lineWidth = 2.5;
 
@@ -13,11 +16,12 @@ function startPainting(){
 function onMouseMove(event) {
     const x = event.offsetX //offset이 canvas와 관련있는 데이터
     const y = event.offsetY
-    if(!painting){
-        ctx.beginPath();
-        ctx.moveTo(x,y)
-    } else {
-        ctx.lineTo(x,y)
+    if(!painting){ // 클릭을 하지 않은 상태에서는
+        ctx.beginPath(); // 새로운 경로를 설정
+        ctx.moveTo(x,y) // 경로의 시작점이 계속 바뀜. (x, y)로.
+    } else { // 클릭을 한 상태로 움직이면
+        ctx.lineTo(x,y) // 설정된 경로의 마지막 점 위치까지 x,y 까지 직선을 설정한다.
+        ctx.stroke(); // 직선이 설정되고 나서 선을 그린다.
     }
 }
 
