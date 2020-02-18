06406321bfe6c14158553e5367eba4329717cb8c
권태일 <xodlf0415@jmsight.com>
2020-02-18 12:54:32 +0900

    캔버스의 context를 2d로 설정

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const canvas = document.getElementById('jsCanvas')
+const ctx = canvas.getContext("2d")
+
+ctx.strokeStle="#2c2c2c"
+ctx.lineWidth = 2.5;
 
 let painting = false;
+
+function startPainting(){
+
+}
 
 function onMouseMove(event) {
     const x = event.offsetX //offset이 canvas와 관련있는 데이터
     const y = event.offsetY
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y)
+    } else {
+        ctx.lineTo(x,y)
+    }
 }
 
 function onMouseDown(event){
