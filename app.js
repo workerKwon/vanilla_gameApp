0fda236708529ae239c1a033e046fec599469a2a
권태일 <xodlf0415@jmsight.com>
2020-02-19 12:43:15 +0900

    filling 기능 추가, filling과 painting 기능 변경 추가

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const canvas = document.getElementById('jsCanvas')
 const ctx = canvas.getContext("2d")
 const colors = document.getElementsByClassName('js-color')
 const range = document.getElementById('jsRange')
+const mode = document.getElementById('jsMode')
 
-canvas.width = 700; // context함수를 이용하기 위해서는 css가 아닌 element에 사이즈를 줘야한다.
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c"
+const CANVAS_SIZE = 700
 
-ctx.strokeStyle="#2c2c2c"
+canvas.width = CANVAS_SIZE; // context함수를 이용하기 위해서는 css가 아닌 element에 사이즈를 줘야한다.
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR
+ctx.fillStyle = INITIAL_COLOR
 ctx.lineWidth = 2.5;
 
+// 그리기 기능을 실행할지 안할지 구분하기 위한 변수
 let painting = false;
+
+// 채우기 기능을 사용할지 안할지를 구분하기 위한 변수
+let filling = false;
 
 function startPainting(){
 
@@ -43,6 +52,7 @@ function onMouseLeave(event){
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color
+    ctx.fillStyle = color
 }
 
 // event.target.value를 가져와 canvas context의 lineWidth를 변경한다.
@@ -51,11 +61,31 @@ function handleRangeChange(event){
     ctx.lineWidth = size
 }
 
+// #jsMode를 클릭하면 fill 기능과 paint 기능을 바꾼다.
+function handleModeClick(event){
+    if(filling === true){ // click 했을 때 filling이 true면 false로 바꾸고 text를 FILL로 변경
+        filling = false
+        mode.innerText = "Fill"
+    } else { // click 했을 때 filling이 false면 true로 바꾸고 text를 PAINTING으로 변경
+        filling = true
+        mode.innerText = "Painting"
+    }
+}
+
+// 캔버스를 클릭하면 실행되는 함수
+function handleCanvasClick(){
+    if(filling){ //filling이 true일 때
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+        // (0,0)위치에서부터 (canvas.width, canvas.height)사이즈 만큼 사각형으로 채운다.
+    }
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove)
     canvas.addEventListener('mousedown', onMouseDown) //그리기 시작 용도
     canvas.addEventListener('mouseup', onMouseUp) // 그리기 멈춤 용도
     canvas.addEventListener('mouseleave', onMouseLeave)
+    canvas.addEventListener('click', handleCanvasClick)
 }
 
 // .js-color를 array로 만든 후 각각에 click event를 추가한다.
@@ -63,7 +93,12 @@ Array.from(colors).forEach(color =>
     color.addEventListener("click", handleColorClick)
 );
 
-// #jsRange에 값이 들어갈 때
+// #jsRange에 값이 들어갈 때 기능 실행
 if(range){
     range.addEventListener("input", handleRangeChange)
+}
+
+// #jsMode에 클릭 기능 추가
+if(mode){
+    mode.addEventListener('click', handleModeClick)
 }
